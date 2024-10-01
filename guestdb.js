6cc11f87dcf0c80e92f9d6f3d08135a59781a79a
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-10-01 10:07:30 +0530

    guestdb.js

--- guestdb.js
+++ guestdb.js
@@ -1,12 +1,32 @@
+const chalk=require("chalk");
 //create
-const add =()=>{
-    return "adding";
-}
+/*const add =()=>{
+   console.log(chalk.green("adding"));
+}*/
 
 //view
-const view =()=> "view"  //short hand of arrow function
+/*const view =()=> console.log(chalk.blue("view"));  //short hand of arrow function*/
 
-module.exports={
+/*module.exports={
     add,
     view
+}*/
+
+const addGuest =()=>{
+    console.log(chalk.green("Add"));
+}
+const updateGuest =()=>{
+    console.log(chalk.yellow("Update"));
+}
+const deleteGuest =()=>{
+    console.log(chalk.red("Delete"));
+}
+const readGuest =()=>{
+    console.log(chalk.blue("Read"));
+}
+const listGuest =()=>{
+    console.log(chalk.gray("List"));
+}
+module.exports={
+    addGuest,updateGuest,deleteGuest,readGuest,listGuest
 }
