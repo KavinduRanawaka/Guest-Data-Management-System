0ce48533fa4e7c3d877ce34062dc07c30d4a2dbc
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-10-01 21:24:31 +0530

    app

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ db.view();*/
 // }
 yargs.version("1.1.2");
 
+
+//add
 yargs.command({
     command:'add',
     describe:'To add a guest',
@@ -39,22 +41,102 @@ yargs.command({
         address:{
             describe:"Address",
             type:"string"
+        },
+        contact_no:{
+            describe:"Contact No",
+            type:"number"
+        },
+        visit_date:{
+            describe:"Visit Date", 
+            type:"string"
         }
     },
-    handler:function(){
-        db.addGuest();
-        console.log("Name =",yargs.argv.name);
-        console.log("Address =",yargs.argv.address);
+    handler(argv){
+        db.addGuest(argv.name,argv.address,argv.contact_no,argv.visit_date);
+        
     }
 });
+
+//update
 yargs.command({
     command:'update',
     describe:'To update a guest',
-    handler:function(){
-        db.updateGuest();
+    builder:{
+        id:{
+            description:"ID",
+            demandOption:true,
+            type:"number"
+        },
+        name:{
+            describe:"Name",
+            type:"string"
+        },
+        address:{
+            describe:"Address",
+            type:"string"
+        },
+        contact_no:{
+            describe:"Contact No",
+            type:"number"
+        },
+        visit_date:{
+            describe:"Visit Date",
+            type:"string"
+        }
+    },
+    handler(argv){
+        db.updateGuest(argv.id,argv.name,argv.address,argv.contact_no,argv.visit_date);
+        
+    }
+});
+
+//delete
+yargs.command({
+    command:'delete',
+    describe:'To dlete a guest',
+    builder:{
+        id:{
+            description:"ID",
+            demandOption:true,
+            type:"number"
+        }
+    },
+    handler(argv){
+        db.deleteGuest(argv.id);
+        
+    }
+});
+
+//read
+yargs.command({
+    command:'read',
+    describe:'To read a guest',
+    builder:{
+        id:{
+            description:"ID",
+            demandOption:true,
+            type:"number"
+        }
+    },
+    handler(argv){
+        db.readGuest(argv.id);
+        
+    }
+});
+
+//list
+yargs.command({
+    command:'list',
+    describe:'list all guest',
+    handler(argv){
+        db.listGuest();
+        
     }
 });
 
 
-console.log(yargs.argv);
+
+
+
+yargs.parse();
 
