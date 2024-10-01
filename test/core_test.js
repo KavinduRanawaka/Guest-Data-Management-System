1c8293117482a0f47127cff2ee1fabd5f6d28823
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-10-01 21:25:31 +0530

    core_test.js

--- test/core_test.js
+++ test/core_test.js
@@ -1,8 +1,23 @@
 const fs = require('node:fs');
+const os=require('os');
 
 // to write data
 // fs.writeFileSync("data.txt","Hello");
 // fs.appendFileSync("data.txt"," World !")
 
-const readData=fs.readFileSync("data.txt");
-console.log(readData.toString());
+// const readData=fs.readFileSync("data.txt");
+// console.log(readData.toString());
+
+//save
+// const guests = [];
+// guests.push({ name: "name1", address: "add1" });
+
+// // Convert the array to a JSON string before writing
+// const dataJSON=JSON.stringify(guests)
+// fs.writeFileSync("J_data.json",dataJSON);
+
+// //read
+// const dataBuffer=fs.readFileSync("J_data.json")
+// const dataJSON=dataBuffer.toString();
+// const data=JSON.parse(dataBuffer);
+// console.log(data);
