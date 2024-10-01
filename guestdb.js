7ca2f27adb22426ee4ef8a3f5f1c2ca21aa64932
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-10-01 21:25:13 +0530

    guestdb.js

--- guestdb.js
+++ guestdb.js
@@ -1,4 +1,6 @@
+const fs=require("fs");
 const chalk=require("chalk");
+
 //create
 /*const add =()=>{
    console.log(chalk.green("adding"));
@@ -11,22 +13,55 @@ const chalk=require("chalk");
     add,
     view
 }*/
+const db_file="data.json";
 
-const addGuest =()=>{
-    console.log(chalk.green("Add"));
+const addGuest =(name,address,contact_no,visit_date)=>{
+    const guests=loadGuests();
+    guests.push({
+        name:name,
+        address:address,
+        contact_no:contact_no,
+        visit_date:visit_date
+    });
+
+    saveGuest(guests);
+
+    console.log(chalk.green("Data saved!"));
 }
-const updateGuest =()=>{
-    console.log(chalk.yellow("Update"));
+const updateGuest =(id)=>{
+    console.log(chalk.yellow("Update",id));
 }
-const deleteGuest =()=>{
-    console.log(chalk.red("Delete"));
+const deleteGuest =(id)=>{
+    console.log(chalk.red("Delete",id));
 }
-const readGuest =()=>{
-    console.log(chalk.blue("Read"));
+const readGuest =(id)=>{
+    console.log(chalk.blue("Read",id));
 }
-const listGuest =()=>{
-    console.log(chalk.gray("List"));
+const listGuest =(id)=>{
+    console.log(chalk.gray("List",id));
+}
+
+
+const saveGuest=(guests)=>{
+    const dataJSON=JSON.stringify(guests);
+    fs.writeFileSync(db_file,dataJSON);
+}
+
+const loadGuests=()=>{
+    try{
+
+    }catch(e){
+     
+    }
+    const dataBuffer=fs.readFileSync(db_file);
+    const dataJSON=dataBuffer.toString();
+    const data = JSON.parse(dataJSON);
+    return data;
 }
 module.exports={
-    addGuest,updateGuest,deleteGuest,readGuest,listGuest
-}
+    addGuest,
+    updateGuest,
+    deleteGuest,
+    readGuest,
+    listGuest
+};
